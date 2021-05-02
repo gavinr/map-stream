4980d27f9fd80290106bb25d0b8e96363e61e9f6
Gavin Rehkemper <grehkemper@esri.com>
2021-05-01 21:03:21 -0500

    in progress

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,21 @@
 <script lang="ts">
-	export let name: string;
+  import { onMount } from "svelte";
+  import { ISearchOptions, searchItems } from "@esri/arcgis-rest-portal";
+
+  export let name: string;
+
+  onMount(async () => {
+    const searchOptions: ISearchOptions = {
+      q: "test",
+    };
+    const items = await searchItems(searchOptions);
+    console.log("items", items);
+  });
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Map Stream</h1>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
