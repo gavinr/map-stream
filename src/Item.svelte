9319aa4eb7604a28381102d98a35b5707a96b028
Gavin Rehkemper <gavin@gavinr.com>
2021-05-02 16:51:40 -0500

    refactor

---
+++ src/Item.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import type { IItem } from "@esri/arcgis-rest-portal";
+
+  export let item: IItem;
+</script>
+
+<div class="item">
+  <a
+    href={`https://www.arcgis.com/apps/mapviewer/index.html?webmap=${item.id}`}
+    target="_blank"
+    ><img
+      src={`https://www.arcgis.com/sharing/rest/content/items/${item.id}/info/${item.thumbnail}`}
+      alt="Map thumbnail"
+    /></a
+  >
+
+  Title:
+  <a
+    href={`https://www.arcgis.com/apps/mapviewer/index.html?webmap=${item.id}`}
+    target="_blank">{item.title}</a
+  ><br />
+  Owner: {item.owner}<br />
+  Modified: {new Date(item.modified)}
+  <div class="clear" />
+</div>
+
+<style>
+  .item {
+    padding: 10px;
+    border: 1px solid gray;
+    margin: 10px;
+  }
+  .item img {
+    float: left;
+    padding-right: 10px;
+  }
+  .clear {
+    clear: both;
+  }
+</style>
--- src/UpdatingMapList.svelte
+++ src/UpdatingMapList.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import { searchItems } from "@esri/arcgis-rest-portal";
   import type { ISearchOptions, IItem } from "@esri/arcgis-rest-portal";
+  import Item from "./Item.svelte";
 
   let items: IItem[];
   $: items = [];
@@ -20,38 +21,8 @@
 
 <!-- `https://www.arcgis.com/home/item.html?id=${item.id}` -->
 {#each items as item}
-  <div class="item">
-    <a
-      href={`https://www.arcgis.com/apps/mapviewer/index.html?webmap=${item.id}`}
-      target="_blank"
-      ><img
-        src={`https://www.arcgis.com/sharing/rest/content/items/${item.id}/info/${item.thumbnail}`}
-        alt="Map thumbnail"
-      /></a
-    >
-
-    Title:
-    <a
-      href={`https://www.arcgis.com/apps/mapviewer/index.html?webmap=${item.id}`}
-      target="_blank">{item.title}</a
-    ><br />
-    Owner: {item.owner}<br />
-    Modified: {new Date(item.modified)}
-    <div class="clear" />
-  </div>
+  <Item {item} />
 {/each}
 
 <style>
-  .item {
-    padding: 10px;
-    border: 1px solid gray;
-    margin: 10px;
-  }
-  .item img {
-    float: left;
-    padding-right: 10px;
-  }
-  .clear {
-    clear: both;
-  }
 </style>
