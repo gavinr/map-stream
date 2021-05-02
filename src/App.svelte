8398b99621745e8112810657a74a2a9b261fc6a3
Gavin Rehkemper <gavin@gavinr.com>
2021-05-02 16:22:03 -0500

    show items

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,62 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { searchItems } from "@esri/arcgis-rest-portal";
-  import type { ISearchOptions } from "@esri/arcgis-rest-portal";
+  import type { ISearchOptions, IItem } from "@esri/arcgis-rest-portal";
 
-  export let name: string;
+  let items: IItem[];
+  $: items = [];
 
   onMount(async () => {
     const searchOptions: ISearchOptions = {
-      q: "test",
+      q: '((type: "Web Map")) AND NOT type:"Web Mapping Application"',
+      sortField: "modified",
+      sortOrder: "desc",
     };
-    const items = await searchItems(searchOptions);
+    const searchItemsResults = await searchItems(searchOptions);
+    items = searchItemsResults.results;
     console.log("items", items);
   });
 </script>
 
 <main>
   <h1>Map Stream</h1>
+  <div>
+    <!-- `https://www.arcgis.com/home/item.html?id=${item.id}` -->
+    {#each items as item}
+      <div class="item">
+        <a
+          href={`https://www.arcgis.com/apps/mapviewer/index.html?webmap=${item.id}`}
+          target="_blank"
+          ><img
+            src={`https://www.arcgis.com/sharing/rest/content/items/${item.id}/info/${item.thumbnail}`}
+            alt="Map thumbnail"
+          /></a
+        >
+
+        Title:
+        <a
+          href={`https://www.arcgis.com/apps/mapviewer/index.html?webmap=${item.id}`}
+          target="_blank">{item.title}</a
+        ><br />
+        Owner: {item.owner}<br />
+        Modified: {new Date(item.modified)}
+        <div class="clear" />
+      </div>
+    {/each}
+  </div>
 </main>
 
 <style>
+  .item {
+    padding: 10px;
+    border: 1px solid gray;
+    margin: 10px;
+  }
+  .item img {
+    float: left;
+    padding-right: 10px;
+  }
+  .clear {
+    clear: both;
+  }
 </style>
